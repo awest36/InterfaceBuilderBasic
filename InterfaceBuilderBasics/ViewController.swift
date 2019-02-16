9bc8024424a5fd09b48b24bbd50811b6e6a7ed85
Alex <awest36@rocketmail.com>
2019-02-16 13:16:56 -0600

    Initial Commit

---
+++ InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by Alexandria West on 2/16/19.
+//  Copyright © 2019 Alexandria West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
